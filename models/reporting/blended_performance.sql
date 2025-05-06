04a8b8d00068ccd13544c9fed6779d648e5fe4f5
Vincent Gefflaut <vincent.gefflaut@superbolt.agency>
2025-05-06 11:36:47 -0400

    added blended performance table

---
+++ models/reporting/blended_performance.sql
@@ -0,0 +1,48 @@
+{{ config (
+    alias = target.database + '_blended_performance'
+)}}
+
+WITH blended_data as
+    (SELECT channel, date::date, date_granularity, campaign_name, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
+        COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue, 
+        COALESCE(SUM(sho_purchases),0) as sho_purchases, COALESCE(SUM(sho_ft_purchases),0) as sho_ft_purchases, COALESCE(SUM(sho_revenue),0) as sho_revenue, COALESCE(SUM(sho_ft_revenue),0) as sho_ft_revenue
+    FROM
+        (SELECT 'Meta' as channel, date, date_granularity, 
+            campaign_name, spend, link_clicks as clicks, impressions, purchases as paid_purchases, revenue as paid_revenue,
+            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
+        FROM {{ source('reporting','facebook_ad_performance') }}
+        UNION ALL
+        SELECT 'Google Ads' as channel, date, date_granularity,
+            campaign_name, spend, clicks, impressions, purchases as paid_purchases, revenue as paid_revenue,
+            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
+        FROM {{ source('reporting','googleads_campaign_performance') }}
+        UNION ALL
+        SELECT 'TikTok' as channel, date, date_granularity,
+            campaign_name, spend, clicks, impressions, purchases as paid_purchases, revenue as paid_revenue,
+            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
+        FROM {{ source('reporting','tiktok_ad_performance') }}
+        WHERE source_medium IN ('google / cpc','Facebook / paidsocial')
+        UNION ALL
+        SELECT 'Shopify' as channel, date, date_granularity,
+        null as campaign_name, 0 as spend, 0 as clicks, 0 as impressions, 0 as paid_purchases, 0 as paid_revenue,
+            orders as sho_purchases, first_orders as sho_ft_purchases, total_net_sales as sho_revenue, first_order_total_net_sales as sho_ft_revenue
+        FROM {{ source('reporting','shopify_sales') }}
+        )
+    GROUP BY channel, date, date_granularity, campaign_name)
+    
+SELECT channel,
+    date,
+    date_granularity,
+    campaign_name,
+    spend,
+    clicks,
+    impressions,
+    paid_purchases,
+    paid_revenue,
+    sho_purchases,
+    sho_ft_purchases,
+    sho_revenue,
+    sho_ft_revenue,
+    ga4_purchases, 
+    ga4_revenue
+FROM blended_data
