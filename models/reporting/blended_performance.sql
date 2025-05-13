5c5ffe74a8348e6fdafea523d9a37ae2af081ac1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-05-13 11:46:47 +0200

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -20,37 +20,6 @@ with shopify_orders_filtered as (
     )
 
 )
-
-, unioned as (
-
-    {% set granularities = ['day', 'week', 'month', 'quarter', 'year'] %}
-
-    {% for granularity in granularities %}
-    
-    select 
-        'Shopify' as channel,
-        date_trunc('{{ granularity }}', date) as date,
-        '{{ granularity }}' as date_granularity,
-        null as campaign_name,
-        sum(0) as spend,
-        sum(0) as clicks,
-        sum(0) as impressions,
-        sum(0) as paid_purchases,
-        sum(0) as paid_revenue,
-        count(*) as sho_purchases,
-        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
-        sum(subtotal_revenue) as sho_revenue,
-        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
-    from shopify_orders_filtered
-    group by 1, 2, 3
-
-    {% if not loop.last %}
-    union all
-    {% endif %}
-
-    {% endfor %}
-
-)
     
 , blended_data as
     (SELECT channel, date::date, date_granularity, campaign_name, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
@@ -72,9 +41,90 @@ with shopify_orders_filtered as (
             0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
         FROM {{ source('reporting','tiktok_ad_performance') }}
         UNION ALL
-        SELECT * FROM unioned
-        )
-        )
+        select 
+        'Shopify' as channel,
+        date_trunc('day', date) as date,
+        'day' as date_granularity,
+        null as campaign_name,
+        sum(0) as spend,
+        sum(0) as clicks,
+        sum(0) as impressions,
+        sum(0) as paid_purchases,
+        sum(0) as paid_revenue,
+        count(*) as sho_purchases,
+        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
+        sum(subtotal_revenue) as sho_revenue,
+        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        from shopify_orders_filtered
+        group by 1, 2, 3
+        UNION ALL
+        select 
+        'Shopify' as channel,
+        date_trunc('week', date) as date,
+        'week' as date_granularity,
+        null as campaign_name,
+        sum(0) as spend,
+        sum(0) as clicks,
+        sum(0) as impressions,
+        sum(0) as paid_purchases,
+        sum(0) as paid_revenue,
+        count(*) as sho_purchases,
+        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
+        sum(subtotal_revenue) as sho_revenue,
+        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        from shopify_orders_filtered
+        group by 1, 2, 3
+        UNION ALL
+        select 
+        'Shopify' as channel,
+        date_trunc('month', date) as date,
+        'month' as date_granularity,
+        null as campaign_name,
+        sum(0) as spend,
+        sum(0) as clicks,
+        sum(0) as impressions,
+        sum(0) as paid_purchases,
+        sum(0) as paid_revenue,
+        count(*) as sho_purchases,
+        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
+        sum(subtotal_revenue) as sho_revenue,
+        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        from shopify_orders_filtered
+        group by 1, 2, 3
+        UNION ALL
+        select 
+        'Shopify' as channel,
+        date_trunc('quarter', date) as date,
+        'quarter' as date_granularity,
+        null as campaign_name,
+        sum(0) as spend,
+        sum(0) as clicks,
+        sum(0) as impressions,
+        sum(0) as paid_purchases,
+        sum(0) as paid_revenue,
+        count(*) as sho_purchases,
+        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
+        sum(subtotal_revenue) as sho_revenue,
+        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        from shopify_orders_filtered
+        group by 1, 2, 3
+        UNION ALL
+        select 
+        'Shopify' as channel,
+        date_trunc('year', date) as date,
+        'year' as date_granularity,
+        null as campaign_name,
+        sum(0) as spend,
+        sum(0) as clicks,
+        sum(0) as impressions,
+        sum(0) as paid_purchases,
+        sum(0) as paid_revenue,
+        count(*) as sho_purchases,
+        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
+        sum(subtotal_revenue) as sho_revenue,
+        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        from shopify_orders_filtered
+        group by 1, 2, 3)
     GROUP BY channel, date, date_granularity, campaign_name)
     
 SELECT channel,
