95b006f1723be196e4d46e7a2c4946e4eae9f683
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-05-14 10:05:10 -0600

    Update blended_performance.sql
    sho_revenue_excl_toners

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -24,22 +24,28 @@ with shopify_orders_filtered as (
 , blended_data as
     (SELECT channel, date::date, date_granularity, campaign_name, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
         COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue, 
-        COALESCE(SUM(sho_purchases),0) as sho_purchases, COALESCE(SUM(sho_ft_purchases),0) as sho_ft_purchases, COALESCE(SUM(sho_revenue),0) as sho_revenue, COALESCE(SUM(sho_ft_revenue),0) as sho_ft_revenue
+        COALESCE(SUM(sho_purchases),0) as sho_purchases, COALESCE(SUM(sho_ft_purchases),0) as sho_ft_purchases, COALESCE(SUM(sho_revenue),0) as sho_revenue, 
+        COALESCE(SUM(sho_ft_revenue),0) as sho_ft_revenue, COALESCE(SUM(sho_revenue_excl_toners),0) as sho_revenue_excl_toners
     FROM
         (SELECT 'Meta' as channel, date, date_granularity, 
             campaign_name, spend, link_clicks as clicks, impressions, purchases as paid_purchases, revenue as paid_revenue,
-            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
+            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue, 0 as sho_revenue_excl_toners
         FROM {{ source('reporting','facebook_ad_performance') }}
         UNION ALL
         SELECT 'Google Ads' as channel, date, date_granularity,
             campaign_name, spend, clicks, impressions, purchases as paid_purchases, revenue as paid_revenue,
-            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
+            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue, 0 as sho_revenue_excl_toners
         FROM {{ source('reporting','googleads_campaign_performance') }}
         UNION ALL
         SELECT 'TikTok' as channel, date, date_granularity,
             campaign_name, spend, clicks, impressions, purchases as paid_purchases, revenue as paid_revenue,
-            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue
+            0 as sho_purchases, 0 as sho_ft_purchases, 0 as sho_revenue, 0 as sho_ft_revenue, 0 as sho_revenue_excl_toners
         FROM {{ source('reporting','tiktok_ad_performance') }}
+        UNION ALL
+        SELECT 'Shopify' as channel, date, date_granularity,
+            null as campaign_name, 0 as spend, 0 as clicks, 0 as impressions, 0 as paid_purchases, 0 as paid_revenue,
+            orders as sho_purchases, first_orders as sho_ft_purchases, subtotal_sales as sho_revenue, first_order_subtotal_sales as sho_ft_revenue, 0 as sho_revenue_excl_toners
+        FROM {{ source('reporting','shopify_sales') }}
         UNION ALL
         select 
         'Shopify' as channel,
@@ -51,10 +57,11 @@ with shopify_orders_filtered as (
         sum(0) as impressions,
         sum(0) as paid_purchases,
         sum(0) as paid_revenue,
-        count(*) as sho_purchases,
-        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
-        sum(subtotal_revenue) as sho_revenue,
-        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        sum(0) as sho_purchases,
+        sum(0) as sho_ft_purchases,
+        sum(0) as sho_revenue,
+        sum(0) as sho_ft_revenue,
+        sum(subtotal_revenue) as sho_revenue_excl_toners
         from shopify_orders_filtered
         group by 1, 2, 3
         UNION ALL
@@ -68,10 +75,11 @@ with shopify_orders_filtered as (
         sum(0) as impressions,
         sum(0) as paid_purchases,
         sum(0) as paid_revenue,
-        count(*) as sho_purchases,
-        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
-        sum(subtotal_revenue) as sho_revenue,
-        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        sum(0) as sho_purchases,
+        sum(0) as sho_ft_purchases,
+        sum(0) as sho_revenue,
+        sum(0) as sho_ft_revenue,
+        sum(subtotal_revenue) as sho_revenue_excl_toners
         from shopify_orders_filtered
         group by 1, 2, 3
         UNION ALL
@@ -85,10 +93,11 @@ with shopify_orders_filtered as (
         sum(0) as impressions,
         sum(0) as paid_purchases,
         sum(0) as paid_revenue,
-        count(*) as sho_purchases,
-        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
-        sum(subtotal_revenue) as sho_revenue,
-        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        sum(0) as sho_purchases,
+        sum(0) as sho_ft_purchases,
+        sum(0) as sho_revenue,
+        sum(0) as sho_ft_revenue,
+        sum(subtotal_revenue) as sho_revenue_excl_toners
         from shopify_orders_filtered
         group by 1, 2, 3
         UNION ALL
@@ -102,10 +111,11 @@ with shopify_orders_filtered as (
         sum(0) as impressions,
         sum(0) as paid_purchases,
         sum(0) as paid_revenue,
-        count(*) as sho_purchases,
-        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
-        sum(subtotal_revenue) as sho_revenue,
-        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        sum(0) as sho_purchases,
+        sum(0) as sho_ft_purchases,
+        sum(0) as sho_revenue,
+        sum(0) as sho_ft_revenue,
+        sum(subtotal_revenue) as sho_revenue_excl_toners
         from shopify_orders_filtered
         group by 1, 2, 3
         UNION ALL
@@ -119,10 +129,11 @@ with shopify_orders_filtered as (
         sum(0) as impressions,
         sum(0) as paid_purchases,
         sum(0) as paid_revenue,
-        count(*) as sho_purchases,
-        sum(case when customer_order_index = 1 then 1 else 0 end) as sho_ft_purchases,
-        sum(subtotal_revenue) as sho_revenue,
-        sum(case when customer_order_index = 1 then subtotal_revenue else 0 end) as sho_ft_revenue
+        sum(0) as sho_purchases,
+        sum(0) as sho_ft_purchases,
+        sum(0) as sho_revenue,
+        sum(0) as sho_ft_revenue,
+        sum(subtotal_revenue) as sho_revenue_excl_toners
         from shopify_orders_filtered
         group by 1, 2, 3)
     GROUP BY channel, date, date_granularity, campaign_name)
@@ -139,5 +150,6 @@ SELECT channel,
     sho_purchases,
     sho_ft_purchases,
     sho_revenue,
-    sho_ft_revenue
+    sho_ft_revenue,
+    sho_revenue_excl_toners
 FROM blended_data
